eeb2625a39dbbb5789802ff292ba71855658e3ef
nokranger <anusorn.thavornporn@gmail.com>
2019-03-26 17:36:30 +0700

    components and vue.file

--- vue-webpack/src/main.js
+++ vue-webpack/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Message from './message.vue'
+
+Vue.component('app-message', Message)
 
 new Vue({
   el: '#app',
---
+++ vue-webpack/src/message.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This is a great Message !</h1>
+        <app-input></app-input>
+    </div>
+</template>
+
+<script>
+import Input from './input.vue'
+export default {
+    components : {
+        'app-input' : Input
+    }
+}
+</script>
